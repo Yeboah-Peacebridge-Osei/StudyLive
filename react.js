430766c7e2ac8d4438cffe2f5d8e17c029ed0f51
Peacebridge <73386395+Yeboah-Peacebridge-Osei@users.noreply.github.com>
2023-03-10 11:14:21 -0600

    Add files via upload

---
+++ react.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
